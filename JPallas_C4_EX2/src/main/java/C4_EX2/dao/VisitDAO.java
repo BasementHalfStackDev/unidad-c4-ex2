9be4d7dc53bc400d07d41480447347c4bed18c02
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-20 10:55:02 +0200

    Visit DAO find by client and odontologist

--- JPallas_C4_EX2/src/main/java/C4_EX2/dao/VisitDAO.java
+++ JPallas_C4_EX2/src/main/java/C4_EX2/dao/VisitDAO.java
@@ -3,10 +3,16 @@
  */
 package C4_EX2.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import C4_EX2.dto.Visit;
 
 public interface VisitDAO extends JpaRepository<Visit, Long> {
+
+	public List<Visit> findVisitsByClientId(Long clientId);
+
+	public List<Visit> findVisitsByOdontologistId(Long odontologistId);
 
 }

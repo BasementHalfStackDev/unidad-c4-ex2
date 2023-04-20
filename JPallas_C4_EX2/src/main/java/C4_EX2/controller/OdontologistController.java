a14146fd2535da1a8b5ac9202a4c91bd4366fc6d
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-20 10:25:25 +0200

    Created structure

---
+++ JPallas_C4_EX2/src/main/java/C4_EX2/controller/OdontologistController.java
@@ -0,0 +1,8 @@
+/**
+ * @author BasementHalfStackDev/Josep Maria Pallas Batalla
+ */
+package C4_EX2.controller;
+
+public class OdontologistController {
+
+}
---
+++ JPallas_C4_EX2/src/main/java/C4_EX2/dto/Visit.java
@@ -0,0 +1,8 @@
+/**
+ * @author BasementHalfStackDev/Josep Maria Pallas Batalla
+ */
+package C4_EX2.dto;
+
+public class Visit {
+
+}
